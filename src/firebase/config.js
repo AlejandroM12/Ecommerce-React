123409dae7ca9ae1aafb088374cdf2cdc514759a
alejandro <alejandro.morel1905@gmail.com>
2023-09-01 17:56:55 -0300

    update config firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfp3KyGVoLn7oxY0YDF4Ow5IhwxJElVqU",
-  authDomain: "ecommerce-react-c3a4b.firebaseapp.com",
-  projectId: "ecommerce-react-c3a4b",
-  storageBucket: "ecommerce-react-c3a4b.appspot.com",
-  messagingSenderId: "1010987140615",
-  appId: "1:1010987140615:web:132f16739e62ab00610ca2",
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: 'ecommerce-react-c3a4b',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 
 // Initialize Firebase
